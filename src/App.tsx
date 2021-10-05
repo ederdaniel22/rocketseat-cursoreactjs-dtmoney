8331f6f091f86d627cb7fd129bc992bb87ce8d0f
EDER DANIEL DOMINGUES <60990935+ederdaniel22@users.noreply.github.com>
2021-10-05 10:18:38 -0400

    Criação e estilização dos componentes: Dashboard, Header e Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-font-size: 64px;
-color: #8257e6;
-`
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Olá Meu Deus</Title>
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
   );
 }
