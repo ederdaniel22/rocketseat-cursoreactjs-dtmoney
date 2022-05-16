b9f67bbebbe9705bd9c557a1400819216a6a239e
EDER DANIEL DOMINGUES <60990935+ederdaniel22@users.noreply.github.com>
2022-05-16 19:44:33 -0400

    Criação e estilização dos buttons do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Modal from "react-modal";
 import closeImg from '../../assets/close.svg'
-import { Container } from "./styles";
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -8,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposito');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -32,6 +37,29 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           type="number"
           placeholder="Valor"
         />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => { setType('deposito'); }}
+            isActive={type === 'deposito'}
+            activeColor='green'
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+          <RadioBox
+            type="button"
+            onClick={() => { setType('withdraw'); }}
+            isActive={type === 'withdraw'}
+            activeColor='red'
+
+          >
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saida</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
         <input
           placeholder="Categoria"
         />
