ed66102267c9e14f069d826c9bf330f1344f2ed3
EDER DANIEL DOMINGUES <60990935+ederdaniel22@users.noreply.github.com>
2022-05-22 16:09:19 -0400

    Final das tabelas e rotas de comunicação

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
+
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -19,8 +21,9 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
 
       <NewTransactionModal
@@ -29,6 +32,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
